94f4b0e61710f8d2c56c8a002e5c12c2a9189672
99 <99@qq.com>
2024-08-27 09:44:47 +0800

    1

--- syncEnv.js
+++ syncEnv.js
@@ -6,10 +6,10 @@ const {
 } = require('./quantum_syncEnv');
 
 !(async () => {
-    if (! process.env.IsSystem == "true") {
+    if (process.env.IsSystem != "true") {
         await sendNotify("开始同步环境变量了，可能要点时间，骚等一下。", true)
     }
-    await syncEnvs(!isSystem);
+    await syncEnvs(process.env.IsSystem != "true");
 })().catch((e) => {
     console.log("脚本异常：" + e);
 });
